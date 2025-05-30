6650dda79a8761784cbad09506947b2bb6316983
Teja Chilumula <tejachilumula07@gmail.com>
2025-05-30 01:54:10 -0700

    changes to Majority-element-2

--- Bit-Manipulations/229. Majority Element 2.cpp
+++ Bit-Manipulations/229. Majority Element 2.cpp
@@ -11,32 +11,52 @@ public:
 
         // Boyer Moore's Algo
         
-        int c1 = 0, c2 = 0, cnt1 = 0, cnt2 = 0, n = nums.size();
+        class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+         
+        int c1 = 0, c2 = 1, cnt1 = 0, cnt2 = 0, n = nums.size();
 
         for(int i=0;i<nums.size();i++){
-            if(cnt1 == 0){
-                cnt1 = 1;
-                c1 = nums[i];
-            }
-            else if(cnt2 == 0){
-                cnt2 = 1;
-                c2 = nums[i];
-            }
-            else if(nums[i] == c1){
+            
+        
+            if(nums[i] == c1){
                 cnt1++;
             }
             else if(nums[i] == c2){
                 cnt2++;
             }
+            else if(cnt1 == 0){
+                c1 = nums[i];
+                cnt1    = 1;
+            }
+            else if(cnt2 == 0){
+                c2 = nums[i];
+                cnt2 = 1;
+            }
             else{
                 cnt1 -= 1;
                 cnt2 -= 1;
             }
+        }
             vector<int> res;
+            
+            /* this wont work
+            if(cnt1 > n/3) res.push_back(c1);
+            if(cnt2 > n/3) res.push_back(c2);
+             for case [2,2,1,3] both for cnt1 = 1 and cnt2 = 0 at the end becz it is dynamic to the values, so we need the cross check with the obtained elements count
+            
+            cnt1 = 0, cnt2 = 0;
+            for(int i=0;i<n;i++){
+                if(nums[i] == c1) cnt1++;
+                else if(nums[i] == c2) cnt2++;
+            }*/
             
             if(cnt1 > n/3) res.push_back(c1);
             if(cnt2 > n/3) res.push_back(c2);
 
             return res;
+    }
+};
         }
         };
