7c50dbbb3a7deaf6c265a415a92f3a6bfb3c6ab7
Teja Chilumula <tejachilumula07@gmail.com>
2025-06-29 02:19:56 -0700

    Added 2405.LC to Array Folder

---
+++ Arrays/Sliding_Window_Tricky/2405. Optimal Partition of String.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int partitionString(string s) {
+        unordered_map<char, int> mp;
+        int n = s.size(), total = 0, start = 0;
+
+        for(int i=0;i<n;i++){
+            mp[s[i]]++;
+    
+            if(mp[s[i]] > 1) {
+                total++;
+                mp.clear();
+                mp[s[i]]++;
+            }
+        }
+
+        return total+1;
+    }
+};
